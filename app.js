3b52fbb467a1d7ace73e9b6f1f36a7cab0edde0b
nnatcha-g <golffywarewolf@gmail.com>
2022-06-20 19:35:14 +0700

    20-6-65

--- app.js
+++ app.js
@@ -17,18 +17,32 @@ function TH_lang(){
 function page_update(){
     switch(lang){
         case 0:
+            // Navigation Bar Language
             document.getElementById("nav-home").innerHTML = "home"
             document.getElementById("nav-about").innerHTML = "about me"
             document.getElementById("nav-portfolio").innerHTML = "portfolio"
             document.getElementById("nav-testimonials").innerHTML = "testimonials"
             document.getElementById("nav-contact").innerHTML = "contact"
+            // Footer Language
+            document.getElementById("footer-home").innerHTML = "home"
+            document.getElementById("footer-about").innerHTML = "about me"
+            document.getElementById("footer-portfolio").innerHTML = "portfolio"
+            document.getElementById("footer-testimonials").innerHTML = "testimonials"
+            document.getElementById("footer-contact").innerHTML = "contact"
             break;
         case 1:
+            // Navigation Bar Language
             document.getElementById("nav-home").innerHTML = "หน้าหลัก"
             document.getElementById("nav-about").innerHTML = "เกี่ยวกับตัวฉัน"
             document.getElementById("nav-portfolio").innerHTML = "ผลงาน"
             document.getElementById("nav-testimonials").innerHTML = "คำรับรอง"
             document.getElementById("nav-contact").innerHTML = "ติดต่อ"
+            // Footer Language
+            document.getElementById("footer-home").innerHTML = "หน้าหลัก"
+            document.getElementById("footer-about").innerHTML = "เกี่ยวกับตัวฉัน"
+            document.getElementById("footer-portfolio").innerHTML = "ผลงาน"
+            document.getElementById("footer-testimonials").innerHTML = "คำรับรอง"
+            document.getElementById("footer-contact").innerHTML = "ติดต่อ"
             break;
     }
 }
